2fb782c256e5e29c3f949885b91276642b5bef68
Rohan <RohanGforwork@gmail.com>
2024-01-26 23:21:48 +0530

    added App.js changes with Homepage and community

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/profile" element={<Profile/>} />
+        <Route path="/HomePage" element={<HomePage/>} />
+        <Route path="/Community" element={<Community/>} />
         
       </Routes>
     </BrowserRouter>
