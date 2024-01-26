d4c68110a3969b505477e6532cbe3c239d86feea
Abhideep Kumar <89687368+abhideepkumar@users.noreply.github.com>
2024-01-27 00:10:49 +0530

    Revert "Feature branch"

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -4,7 +4,7 @@ import { faGoogle, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 export default function Login() {
   const onSubmit = (event) => {
     event.preventDefault();
-    window.location.href = "/home";
+    window.location.href = "/profile";
   };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
