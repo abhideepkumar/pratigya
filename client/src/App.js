df712114eefd1c19027d0742b5ecf700c9bb8aad
Rohan <RohanGforwork@gmail.com>
2024-01-26 23:04:41 +0530

    added new images ,cant figure out the linking

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Login from './components/login';
 import Profile from './components/profile';
 import HomePage from './components/HomePage';  // Import the HomePage component
-import Community from './components/community'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -12,8 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/profile" element={<Profile/>} />
-        <Route path='/community' element={<Community/>} />
-        <Route path='/HomePage' element={<HomePage/>}/>
+        
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/common/Scrollablecomponents/scrollablecomponents.js
+++ client/src/components/common/Scrollablecomponents/scrollablecomponents.js
@@ -8,11 +8,21 @@ const ScrollableSections = () => {
         <h2>Reported By You</h2>
         <div className="scrollable-content">
           {/* Five pictures for Section 1 */}
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=58-631&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 1" />
+          </div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=58-613&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 2" />
+          </div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=58-595&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 3" />
+          </div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=58-577&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 4" />
+          </div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=58-631&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 5" />
+          </div>
         </div>
       </div>
 
@@ -20,11 +30,21 @@ const ScrollableSections = () => {
         <h2>Recent near You</h2>
         <div className="scrollable-content">
           {/* Five pictures for Section 2 */}
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=50-436&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 6" />
+          </div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=50-434&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 7" />
+          </div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=50-455&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 8" />
+          </div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=50-434&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 9" />
+          </div>
+          <div className="image">
+            <img src="https://www.figma.com/file/rZBEWKCGRrqFtkrn69rBFx/SIH-app?type=design&node-id=50-473&mode=design&t=93rxjSYQ1Zh6WKg2-4" alt="Image 10" />
+          </div>
         </div>
       </div>
     </div>
