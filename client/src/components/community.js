1903901f6349aeb566ce445eae90cd19e5d032df
Abhideep Kumar <itsabhideep@gmail.com>
2024-02-19 20:00:18 +0530

    fixed community page errors

--- client/src/components/community.js
+++ client/src/components/community.js
@@ -1,40 +1,44 @@
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShare, faEllipsis, faUpLong, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import { React, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 import { getFirestore, addDoc, getDocs, collection } from "firebase/firestore";
+
 const Post = () => {
-  const [post, setPost] = useState({ caption: "", post: "" });
+  const [post, setPost] = useState({ incident: "", description: "" });
   const [allposts, setAllposts] = useState([]);
   const db = getFirestore();
+
   useEffect(() => {
     const usersPosts = async () => {
+      const toastLoad = toast.loading("Loading Posts...");
       const querySnapshot = await getDocs(collection(db, "posts"));
-      querySnapshot.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
-        setAllposts((prev) => [...prev, doc.data()]);
-      });
+      const postsData = querySnapshot.docs.map((doc) => doc.data());
+      setAllposts(postsData);
+      toast.dismiss(toastLoad);
     };
     usersPosts();
   }, [db]);
 
-  const Post = async () => {
-    if (post.caption === "" || post.post === "") return toast.error(`Inputs can't be empty`);
-    const postsRef = collection(db, "posts");
-    const postRef = await addDoc(postsRef, {
+  const handlePost = async () => {
+    if (post.incident === "" || post.description === "") {
+      return toast.error(`Inputs can't be empty`);
+    }
+    const newPost = {
       author: localStorage.getItem("name"),
-      incident: post.caption,
-      description: post.post,
+      incident: post.incident,
+      description: post.description,
       position: "Pratigya User",
       upvotes: Math.floor(Math.random() * 20) + 1,
       time: Math.floor(Math.random() * 10) + 1,
       emojis: Math.floor(Math.random() * 50) + 1,
-    }).then(() => {
-      toast.success("Post uploaded");
-      setAllposts((prev) => [...prev, post]);
-      setPost({ caption: "", post: "" });
-    });
+    };
+    await addDoc(collection(db, "posts"), newPost);
+    toast.success("Post uploaded");
+    setAllposts((prevPosts) => [...prevPosts, newPost]);
+    setPost({ incident: "", description: "" });
   };
+
   return (
     <main>
       <div className="max-w-xl mx-auto px-4 md:px-0">
@@ -44,48 +48,43 @@ const Post = () => {
           </a>
           <span className="px-5">Community</span>
         </p>
-        {/* User Post input UI */}
-        <div className=" p-6 m-4 bg-blue-100 rounded-lg">
+        <div className="p-6 m-4 bg-blue-100 rounded-lg">
           <div className="flex items-center mb-4">
             <img src="/img/square.jpg" alt="user" className="w-12 h-12 rounded-full mr-4" />
             <div>
-              <h3 className="text-xl font-semibold text-gray-800">
-                {localStorage.getItem("name")}
-              </h3>
+              <h3 className="text-xl font-semibold text-gray-800">{localStorage.getItem("name")}</h3>
               <p className="text-sm text-gray-500">@{localStorage.getItem("name").toLowerCase()}</p>
             </div>
           </div>
           <div>
             <input
               type="text"
-              placeholder="Your caption here"
+              placeholder="Your incident here"
               className="text-base font-semibold text-gray-800 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              value={post.caption}
-              onChange={(e) => setPost((prevPost) => ({ ...prevPost, caption: e.target.value }))}
+              value={post.incident}
+              onChange={(e) => setPost({ ...post, incident: e.target.value })}
             />
             <input
               type="text"
-              placeholder="type down what you feel"
+              placeholder="Type down what you feel"
               className="text-base text-gray-800 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full h-16"
-              value={post.post}
-              onChange={(e) => setPost((prevPost) => ({ ...prevPost, post: e.target.value }))}
+              value={post.description}
+              onChange={(e) => setPost({ ...post, description: e.target.value })}
             />
           </div>
           <button
             className="bg-blue-500 text-white p-2 px-4 m-1 rounded-md hover:bg-blue-600"
-            onClick={Post}
+            onClick={handlePost}
           >
             Post
           </button>
         </div>
-        {/* Post UI */}
         {allposts.map((post, index) => (
           <div className="p-6" key={index}>
             <div className="flex justify-between items-center">
               <div className="flex items-center mb-4">
-                {/* u can also use src url from post json data */}
                 <img
-                  src={encodeURI(`https://source.unsplash.com/500x500/?indian-girl?${index}`)}
+                  src={encodeURI(`https://source.unsplash.com/200x200/?woman?${index}`)}
                   alt="profile"
                   className="w-12 h-12 rounded-full mr-4"
                 />
@@ -104,9 +103,8 @@ const Post = () => {
                 <p className="text-lg text-gray-600">Recently posted an incident near you</p>
               </div>
               <div className="mb-4 flex justify-center items-center">
-                {/* u can also use src url from post json data */}
                 <img
-                  src={encodeURI(`https://source.unsplash.com/1024x800/?${post.incident}?${index}`)}
+                  src={encodeURI(`https://source.unsplash.com/640x360/?${post.incident}?${index}`)}
                   alt="post"
                   className="h-64 rounded-lg"
                 />
